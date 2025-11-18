e601d06dbd5210c1d5c8a003e997e6d691933ede
idistefano-lang <idistefano@stevejobs.academy>
2025-11-18 11:33:18 +0100

    Add files via upload

---
+++ Progetto8 Ciclo For numeri da 1 a 100.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Somma numeri da 1 a 100  */
+
+int main() {
+	
+	int i;
+	int somma; 
+	
+	for(i=0;i<=100;i=i+1){
+		
+		somma=somma+i;
+		printf("%d\n",somma);
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
