057c20073e6cb21e0e589d443bff660e157e7260
idistefano-lang <idistefano@stevejobs.academy>
2025-11-22 19:17:45 +0100

    Add files via upload

---
+++ Progetto8 Ciclo For numeri a 2 a 2 fino a 100.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int i;
+	
+	for(i=0;i<=100;i=i+2){
+		printf("%d\n",i);
+	}
+	return 0;
+}
---
+++ Progetto9 Ciclo For numeri da 10 a zero.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Ciclo For numeri da 10 a 0  */
+
+int main() {
+	int i;
+	
+	for(i=10;i>=0;i=i-1){
+	 printf("%d\n",i);
+	}
+	
+	
+	
+	return 0;
+}
